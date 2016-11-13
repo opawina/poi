4048f29f1844353a507f3e4e406e6e8793d84425
winana <ztar4e@yandex.ru>
2016-11-13 11:50:36 +0300

    Start again.

---
+++ poi.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP examples</title>
+</head>
+<body>
+	<?php
+		echo "Today is ". date(r). '.';
+	?>
+	<br>
+	<script type="text/javascript">
+		var toddate = new Date();
+		document.write ('Today is ' + toddate);
+	</script>
+</body>
+</html>
